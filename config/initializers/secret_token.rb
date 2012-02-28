37d8a7ad978a34ec524667c5f0304eec7ede4bbd
andy <4001m@mailforspam.com>
2012-02-28 09:27:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogf::Application.config.secret_token = '69f06c55f93ba8343b13a3fe88d5b99ed8f688af70944715b932c21ba21ca6a4637df13da724a65bab7e179970c48269eadc7e5d384d3f631e8474da47e83406'
